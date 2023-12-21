3182c72b2d50e4e26be4df9cc66c732f04ffaad5
AdiFrank1 <Adi.frank@post.runi.ac.il>
2023-12-21 14:19:20 +0200

    GenThree

--- GenThree.java
+++ GenThree.java
@@ -4,6 +4,25 @@
  */
 public class GenThree {
 	public static void main(String[] args) {
-		// Put your code here	
+		double rand = Math.random();
+	int a = Integer.parseInt(args[0]);
+	int b = Integer.parseInt(args[1]);
+
+	int max = Math.max (a,b);
+	int min = Math.min (a,b);
+
+	int num1 = (int) ((Math.random () * (max-min))+min);
+	int num2 = (int) ((Math.random () * (max-min))+min);
+	int num3 = (int) ((Math.random () * (max-min))+min);
+	int minum = Math.min (num1,num2);
+
+	System.out.println(num1);
+	System.out.println(num2);
+	System.out.println(num3);
+
+	if (minum < num3)
+		System.out.println("The min is: " +minum);
+	else
+		System.out.println("The min is: " +num3);	
+		}
 	}
-}
